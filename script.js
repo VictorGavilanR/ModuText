e4399aa94b53690a59d6d62ca47db130e4228584
VictorGavilanR <victormanuelgavilanr18@gmail.com>
2024-10-11 22:48:30 -0300

    login añadido menu hamburguesa
    añadi el login en el menu hamburguesa

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
+let user = document.querySelector('.user'); // Seleccionamos el usuario también
 
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('open');
+    user.classList.toggle('open'); // Añadimos la clase .open a user
 }
+
 
 /*Contador*/
 
