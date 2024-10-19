2b2c52de58f4d01135e22efd2776c9b81286a596
VictorGavilanR <victormanuelgavilanr18@gmail.com>
2024-10-19 17:46:08 -0300

    update contraseña
    ahora la contraseña con la que te registras es la que usas en el login, y la contraseña queda incriptada en la bd

--- controlador/controlador_login.php
+++ controlador/controlador_login.php
@@ -2,7 +2,7 @@
 session_start();
 
 // Incluir la conexión a la base de datos
-include "conexion.php"; // Asegúrate de que esta línea esté incluida
+include "conexion.php";
 
 // Verificar datos con la base de datos
 if (isset($_POST["btningresar"])) {
@@ -11,22 +11,27 @@ if (isset($_POST["btningresar"])) {
         $password = $_POST["password"];
 
         // Preparar la consulta para evitar la inyección SQL
-        $stmt = $conexion->prepare("SELECT * FROM usuarios WHERE rut = ? AND password = ?");
-        $stmt->bind_param("ss", $rut, $password);
+        $stmt = $conexion->prepare("SELECT * FROM usuarios WHERE rut = ?");
+        $stmt->bind_param("s", $rut);
         $stmt->execute();
         $result = $stmt->get_result();
 
         if ($result && $datos = $result->fetch_object()) {
-            // Establecer las variables de sesión con los datos del usuario
-            $_SESSION["id"] = $datos->id;
-            $_SESSION["rut"] = $datos->rut;
-            $_SESSION["nombre"] = $datos->nombre; // Aquí asegúrate que 'nombre' es el campo correcto en tu tabla
+            // Verificar la contraseña cifrada
+            if (password_verify($password, $datos->password)) {
+                // Establecer las variables de sesión con los datos del usuario
+                $_SESSION["id"] = $datos->id;
+                $_SESSION["rut"] = $datos->rut;
+                $_SESSION["nombre"] = $datos->nombre; // Asegúrate de que 'nombre' es el campo correcto en tu tabla
 
-            // Redirigir a la página de retiro
-            header("Location: retiro.php");
-            exit(); // Detener el script después de la redirección
+                // Redirigir a la página de retiro
+                header("Location: retiro.php");
+                exit(); // Detener el script después de la redirección
+            } else {
+                echo "<div>Contraseña incorrecta.</div>";
+            }
         } else {
-            echo "<div>Datos incorrectos o usuario no registrado.</div>";
+            echo "<div>Usuario no registrado.</div>";
         }
 
         // Cerrar la declaración
