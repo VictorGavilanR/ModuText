aa3435d99a236b4aae72f092ba4ab03913a13a68
Luna Le Feuvre <luna.lefeuvre@inacapmail.cl>
2024-11-04 22:27:53 -0300

    fix db y validaciones registro

--- controlador/controlador_registro.php
+++ controlador/controlador_registro.php
@@ -4,82 +4,192 @@ include '../conexion.php';
 
 if ($conexion->connect_error) {
     die("Conexión fallida: " . $conexion->connect_error);
-} else {
-    echo "Conexión exitosa.<br>";
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    
-    // Recibir los datos del formulario
-    $rut = htmlspecialchars($_POST['rut']);
-    $nombre = htmlspecialchars($_POST['nombre']);
-    $apellido_paterno = htmlspecialchars($_POST['app_paterno']);
-    $apellido_materno = htmlspecialchars($_POST['app_materno']);
-    $correo = htmlspecialchars($_POST['correo']);
-    $telefono = htmlspecialchars($_POST['telefono']);
-    $password = htmlspecialchars($_POST['password']);
+    // Recibir tipo de usuario del formulario
     $tipo_usuario = htmlspecialchars($_POST['tipo_usuario']);
+    
+    // Variable de control de errores
+    $errores = false;
 
-    // Inicializar variables adicionales para EMPRESA
-    $rut_cliente = null;
-    $razon_social = null;
-    $fono_cli = null;
-
-    // Si el usuario es empresa, obtenemos datos adicionales
-    if ($tipo_usuario == 'EMPRESA') {
-        $rut_cliente = htmlspecialchars($_POST['rut_cliente']);
-        $razon_social = htmlspecialchars($_POST['razon_social']);
-        $fono_cli = htmlspecialchars($_POST['fono_cli']);
-    }
-
-    // Cifrar la contraseña antes de almacenarla
-    $password_hashed = password_hash($password, PASSWORD_DEFAULT);
-
-    // Insertar datos en la tabla usuarios
-    $stmt = $conexion->prepare("INSERT INTO usuario (rut_usuario, password_usuario, tipo_usuario) VALUES (?, ?, ?)");
-
-    if ($stmt) {
-        $stmt->bind_param("sss", $rut, $password_hashed, $tipo_usuario);
-        
-        if ($stmt->execute()) {
-            echo "Usuario registrado con éxito<br>";
-
-            // Obtener el ID del usuario insertado
-            $id_usuario = $conexion->insert_id;
-
-            // Preparar la consulta para insertar en empresa si el usuario es de tipo EMPRESA
-            if ($tipo_usuario == 'EMPRESA') {
-                $stmt_empresa = $conexion->prepare("INSERT INTO empresa (id_usuario, razon_social, fono_emp, correo_emp) VALUES (?, ?, ?, ?)");
-
-                if ($stmt_empresa) {
-                    $stmt_empresa->bind_param("isss", $id_usuario, $razon_social, $fono_cli, $correo); // Puedes ajustar el binding de acuerdo a tus necesidades
-
-                    if ($stmt_empresa->execute()) {
-                        echo "Cliente registrado con éxito en la tabla empresa.<br>";
-                    } else {
-                        echo "Error al registrar la empresa: " . $stmt_empresa->error;
-                    }
-                    $stmt_empresa->close();
-                } else {
-                    echo "Error en la preparación de la consulta para empresa: " . $conexion->error;
-                }
+    // Verificar tipo de usuario y ejecutar las acciones correspondientes
+    switch ($tipo_usuario) {
+        case 'PARTICULAR':                                      
+            $rut = htmlspecialchars($_POST['rut']);
+            $nombre = htmlspecialchars($_POST['nombre']);
+            $apellido_paterno = htmlspecialchars($_POST['app_paterno']);
+            $apellido_materno = htmlspecialchars($_POST['app_materno']);
+            $correo = htmlspecialchars($_POST['correo']);
+            $fono_per = filter_input(INPUT_POST, 'telefono', FILTER_SANITIZE_NUMBER_INT);
+            $password = htmlspecialchars($_POST['passwordP']);
+            $confirm_password = htmlspecialchars($_POST['confirmPasswordP']);
+            
+            // Verificar campos vacíos y formato de contraseña
+            if (verificarCamposVacios($rut, $nombre, $apellido_paterno, $apellido_materno, $correo, $fono_per, $password, $confirm_password) || 
+                validarContraseñasCoinciden($password, $confirm_password) ||
+                !validarFormatoContraseña($password) ||
+                validarRut($rut,$conexion)) {
+                $errores = true;
             }
 
-            // Establecer un mensaje de éxito y redirigir al login
-            $_SESSION['registro_exitoso'] = "Registro exitoso. Por favor, inicie sesión.";
-            header("Location: ../login.php"); // Asegúrate de que esta ruta sea correcta
-            exit();
+            // Proceder si no hay errores
+            if (!$errores) {
+                $password_hashed = password_hash($password, PASSWORD_DEFAULT);
+                $conexion->begin_transaction();
+                $stmt = $conexion->prepare("INSERT INTO usuario (rut_usuario, password_usuario) VALUES (?, ?)");
+                
+                if ($stmt) {
+                    $stmt->bind_param("ss", $rut, $password_hashed);
+                    
+                    if ($stmt->execute()) {
+                        echo "Usuario registrado con éxito.<br>";
+                        $user_id = $conexion->insert_id;
+                        $stmt->close();
+
+                        // Registrar en la tabla persona_natural
+                        $stmt_particular = $conexion->prepare("INSERT INTO persona_natural (rut_usuario, nombres_per, ap_pat_per, ap_mat_per, fono_per, correo_per) VALUES (?, ?, ?, ?, ?, ?)");
+                        
+                        if ($stmt_particular) {
+                            $stmt_particular->bind_param("ssssis", $rut, $nombre, $apellido_paterno, $apellido_materno, $fono_per, $correo);
+                            
+                            if ($stmt_particular->execute()) {
+                                echo "Cliente registrado con éxito en la tabla persona natural.<br>";
+                                $stmt_particular->close();
+                                $conexion->commit();
+                                $_SESSION['registro_exitoso'] = "Registro exitoso. Por favor, inicie sesión.";
+                                header("Location: ../login.php");
+                                exit();
+                            } else {
+                                echo "Error al registrar la persona natural: " . $stmt_particular->error;
+                        }
+                    } else {
+                        echo "Error en la preparación de la consulta para persona natural: " . $conexion->error;
+                        
+                    }
+                } else {
+                    echo "Error al registrar el usuario: " . $stmt->error;
+                }    
+            } else {
+                echo "Error en la preparación de la consulta para usuarios: " . $conexion->error;
+            }
+            $conexion->rollback();
         } else {
-            echo "Error al registrar el usuario: " . $stmt->error;
+            echo "Errores detectados.";
         }
+        break;
 
-        $stmt->close();
-    } else {
-        echo "Error en la preparación de la consulta para usuarios: " . $conexion->error;
+        case 'EMPRESA':
+            $rut_usuario = htmlspecialchars($_POST['rut_emp']);
+            $razon_social = htmlspecialchars($_POST['razon_social']);
+            $correo = htmlspecialchars($_POST['correoE']);
+            $fono_emp = htmlspecialchars($_POST['fono_emp']);
+            $password = htmlspecialchars($_POST['passwordE']);
+            $confirm_password = htmlspecialchars($_POST['confirmPasswordE']);
+
+            // Verificar campos vacíos, formato de contraseña y rut registrado
+            if (verificarCamposVacios($rut_usuario, $razon_social, $correo, $fono_emp, $password, $confirm_password) ||
+                validarContraseñasCoinciden($password, $confirm_password) ||
+                !validarFormatoContraseña($password) ||
+                validarRut($rut_usuario,$conexion)) {
+                $errores = true;
+            }
+
+            // Proceder si no hay errores
+            if (!$errores) {
+                $password_hashed = password_hash($password, PASSWORD_DEFAULT);
+                $conexion->begin_transaction();
+                $stmt = $conexion->prepare("INSERT INTO usuario (rut_usuario, password_usuario) VALUES (?, ?)");
+                
+                if ($stmt) {
+                    $stmt->bind_param("ss", $rut_usuario, $password_hashed);
+                    
+                    if ($stmt->execute()) {
+                        echo "Usuario registrado con éxito.<br>";
+                        $user_id = $conexion->insert_id;
+                        $stmt->close();
+
+                        // Registrar en la tabla empresa
+                        $stmt_empresa = $conexion->prepare("INSERT INTO empresa (rut_usuario, razon_social, fono_emp, correo_emp) VALUES (?, ?, ?, ?)");
+                        
+                        if ($stmt_empresa) {
+                            $stmt_empresa->bind_param("ssis", $rut_usuario, $razon_social, $fono_emp, $correo);
+
+                            if ($stmt_empresa->execute()) {
+                                echo "Cliente registrado con éxito en la tabla empresa.<br>";
+                                $stmt_empresa->close();
+                                $conexion->commit();
+                                $_SESSION['registro_exitoso'] = "Registro exitoso. Por favor, inicie sesión.";
+                                header("Location: ../login.php");
+                                exit();
+                            } else {
+                                echo "Error al registrar la empresa: " . $stmt_empresa->error;
+                            }
+                        } else {
+                            echo "Error en la preparación de la consulta para empresa: " . $conexion->error;
+                        }
+                    } else {
+                        echo "Error al registrar el usuario: " . $stmt->error;
+                    }
+                    
+                } else {
+                    echo "Error en la preparación de la consulta para usuarios: " . $conexion->error;
+                }
+                $conexion->rollback();
+            } else {
+                echo "Errores detectados.";
+            }
+            break;
+
+        default:
+            echo "Tipo de usuario no válido.";
+            break;
     }
-
-    $conexion->close();
-} else {
-    echo "Todos los campos son obligatorios.";
 }
-?>
+
+// Funciones auxiliares
+
+function verificarCamposVacios(...$campos) {
+    foreach ($campos as $campo) {
+        if (empty($campo)) {
+            echo "Complete los campos vacíos.<br>";
+            return true;
+        }
+    }
+    return false;
+}
+
+function validarContraseñasCoinciden($password, $confirm_password) {
+    if ($password !== $confirm_password) {
+        echo "Las contraseñas no coinciden.<br>";
+        return true;
+    }
+    return false;
+}
+
+function validarFormatoContraseña($password) {
+    if (!preg_match("/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/", $password)) {
+        echo "La contraseña debe tener al menos 8 caracteres, incluyendo al menos una letra y un número.<br>";
+        return false;
+    }
+    return true;
+}
+
+function validarRut($rut, $conexion) {
+    $rut_check = $conexion->prepare("SELECT rut_usuario FROM usuario WHERE rut_usuario = ?");
+    if ($rut_check) {
+        $rut_check->bind_param("s", $rut);
+        $rut_check->execute();
+        $rut_check->store_result();
+        
+        if ($rut_check->num_rows > 0) {
+            $rut_check->close();
+            echo "El rut ya esta registrado.<br>";
+            return true; // El rut ya está registrado
+        }
+        $rut_check->close();
+    } else {
+        echo "Error en la preparación de la consulta: " . $conexion->error;
+    }
+    return false; // El rut no está registrado
+}
--- login.php
+++ login.php
@@ -46,129 +46,128 @@
 
       <!-- Formulario de Registro -->
       <form id="registerForm" class="form-container hidden register-form" action="controlador/controlador_registro.php" method="POST">
-  <h3>Datos del Usuario</h3>
+        <h3>Datos del Usuario</h3>
+        <div class="mb-3">
+          <label for="tipo_usuario" class="form-label">Tipo de Usuario</label>
+          <select class="form-select" name="tipo_usuario" id="tipo_usuario" >
+            <option value="" disabled selected>Seleccione un tipo de usuario</option>
+            <option value="EMPRESA">Empresa</option>
+            <option value="PARTICULAR">Particular</option>
+          </select>
+        </div>
 
-  <div class="mb-3">
-    <label for="tipo_usuario" class="form-label">Tipo de Usuario</label>
-    <select class="form-select" name="tipo_usuario" id="tipo_usuario" required>
-      <option value="" disabled selected>Seleccione un tipo de usuario</option>
-      <option value="EMPRESA">Empresa</option>
-      <option value="PARTICULAR">Particular</option>
-    </select>
-  </div>
+        <!-- Datos de la Empresa -->
+        <div id="empresaContainer" class="empresa-container" style="display: none;">
+          <div id="empresaForm" class="mb-3">
+            <h3>Datos de la Empresa</h3>
+            <div class="mb-3">
+              <label for="rut" class="form-label">RUT de la Empresa</label>
+              <input type="text" name="rut_emp" class="form-control" id="rut" placeholder="Ingrese el RUT de la empresa" >
+              <div id="rutError" style="display:none; color: red;">Rut inválido</div>
+            </div>
+            <div class="mb-3">
+              <label for="razon_social" class="form-label">Razón Social</label>
+              <input type="text" name="razon_social" class="form-control" id="razon_social" placeholder="Ingrese la razón social de la empresa" >
+            </div>
+            <div class="row">
+            <div class="col-md-6 mb-3">
+              <label for="email" class="form-label">Correo Electrónico</label>
+              <input type="email" name="correoE" class="form-control" id="email" placeholder="Ingrese su correo electrónico" >
+            </div>
+            <div class="col-md-6 mb-3">
+              <label for="fono_emp" class="form-label">Teléfono</label>
+              <input type="number" name="fono_emp" class="form-control" id="fono_emp" placeholder="Ingrese su teléfono de contacto" >
+            </div>
+            </div>
+              <div class="mb-3">
+              <label for="passwordE" class="form-label">Contraseña</label>
+              <input type="password" name="passwordE" class="form-control" id="passwordE" placeholder="Ingrese una contraseña" >
+            </div>
+            <div class="mb-3">
+              <label for="confirmPasswordE" class="form-label">Confirmar Contraseña</label>
+              <input type="password" name="confirmPasswordE" class="form-control" id="confirmPasswordE" placeholder="Confirme su contraseña" >
+            </div>
+          </div>
+        </div>
 
-  <!-- Datos de la Empresa -->
-  <div id="empresaContainer" class="empresa-container" style="display: none;">
-    <div id="empresaForm" class="mb-3">
-      <h3>Datos de la Empresa</h3>
-      <div class="mb-3">
-        <label for="rut_cliente" class="form-label">RUT de la Empresa</label>
-        <input type="text" name="rut_cliente" class="form-control" id="rut_cliente" placeholder="Ingrese el RUT de la empresa">
-      </div>
-      <div class="mb-3">
-        <label for="razon_social" class="form-label">Razón Social</label>
-        <input type="text" name="razon_social" class="form-control" id="razon_social" placeholder="Ingrese la razón social de la empresa">
-      </div>
-      <div class="row">
-      <div class="col-md-6 mb-3">
-        <label for="email" class="form-label">Correo Electrónico</label>
-        <input type="email" name="correo" class="form-control" id="email" placeholder="Ingrese su correo electrónico" required>
-      </div>
-      <div class="col-md-6 mb-3">
-        <label for="fonoUser" class="form-label">Teléfono</label>
-        <input type="number" name="telefono" class="form-control" id="fonoUser" placeholder="Ingrese su teléfono de contacto" required>
-      </div>
-    </div>
-      <div class="mb-3">
-      <label for="password" class="form-label">Contraseña</label>
-      <input type="password" name="password" class="form-control" id="password" placeholder="Ingrese una contraseña" required>
-    </div>
-    <div class="mb-3">
-      <label for="confirmPassword" class="form-label">Confirmar Contraseña</label>
-      <input type="password" class="form-control" id="confirmPassword" placeholder="Confirme su contraseña" required>
-    </div>
-  </div>
-  </div>
-
-  <!-- Datos del Particular -->
-  <div id="particularContainer" style="display: none;">
-    <div class="mb-3">
-      <label for="nombre" class="form-label">Nombres</label>
-      <input type="text" name="nombre" class="form-control" id="nombre" placeholder="Ingrese sus nombres" required>
-    </div>
-    <div class="mb-3">
-      <label for="rut" class="form-label">Rut</label>
-      <input type="text" name="rut" class="form-control" id="rut" placeholder="Ingrese rut" required>
-      <div id="rutError" style="display:none; color: red;">Rut inválido</div>
-    </div>
-    <div class="row">
-      <div class="col-md-6 mb-3">
-        <label for="apPat" class="form-label">Apellido Paterno</label>
-        <input type="text" name="app_paterno" class="form-control" id="apPat" placeholder="Ingrese su apellido paterno" required>
-      </div>
-      <div class="col-md-6 mb-3">
-        <label for="apMat" class="form-label">Apellido Materno</label>
-        <input type="text" name="app_materno" class="form-control" id="apMat" placeholder="Ingrese su apellido materno" required>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-md-6 mb-3">
-        <label for="email" class="form-label">Correo Electrónico</label>
-        <input type="email" name="correo" class="form-control" id="email" placeholder="Ingrese su correo electrónico" required>
-      </div>
-      <div class="col-md-6 mb-3">
-        <label for="fonoUser" class="form-label">Teléfono</label>
-        <input type="number" name="telefono" class="form-control" id="fonoUser" placeholder="Ingrese su teléfono de contacto" required>
-      </div>
-    </div>
-    <div class="mb-3">
-      <label for="password" class="form-label">Contraseña</label>
-      <input type="password" name="password" class="form-control" id="password" placeholder="Ingrese una contraseña" required>
-    </div>
-    <div class="mb-3">
-      <label for="confirmPassword" class="form-label">Confirmar Contraseña</label>
-      <input type="password" class="form-control" id="confirmPassword" placeholder="Confirme su contraseña" required>
-    </div>
-  </div>
-      <!-- Contenedor fijo para el botón de registro -->
-  <div class="button-container">
-    <button type="submit" class="btn btn-primary" style="display: none;" id="registerButton">Registrarse</button>
-    <p class="toggle-text">¿Ya tienes cuenta? <a href="#" id="showLogin">Inicia Sesión</a></p>
-  </div>
-</form>
-
-    </form>
+        <!-- Datos del Particular -->
+        <div id="particularContainer" style="display: none;">
+          <div class="mb-3">
+            <label for="nombre" class="form-label">Nombres</label>
+            <input type="text" name="nombre" class="form-control" id="nombre" placeholder="Ingrese sus nombres" >
+          </div>
+          <div class="mb-3">
+            <label for="rut" class="form-label">Rut</label>
+            <input type="text" name="rut" class="form-control" id="rut" placeholder="Ingrese rut" >
+            <div id="rutError" style="display:none; color: red;">Rut inválido</div>
+          </div>
+          <div class="row">
+            <div class="col-md-6 mb-3">
+              <label for="apPat" class="form-label">Apellido Paterno</label>
+              <input type="text" name="app_paterno" class="form-control" id="apPat" placeholder="Ingrese su apellido paterno" >
+            </div>
+            <div class="col-md-6 mb-3">
+              <label for="apMat" class="form-label">Apellido Materno</label>
+              <input type="text" name="app_materno" class="form-control" id="apMat" placeholder="Ingrese su apellido materno" >
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-6 mb-3">
+              <label for="email" class="form-label">Correo Electrónico</label>
+              <input type="email" name="correo" class="form-control" id="email" placeholder="Ingrese su correo electrónico" >
+            </div>
+            <div class="col-md-6 mb-3">
+              <label for="fonoUser" class="form-label">Teléfono</label>
+              <input type="number" name="telefono" class="form-control" id="fonoUser" placeholder="Ingrese su teléfono de contacto" >
+            </div>
+          </div>
+          <div class="mb-3">
+            <label for="passwordP" class="form-label">Contraseña</label>
+            <input type="password" name="passwordP" class="form-control" id="passwordP" placeholder="Ingrese una contraseña" >
+          </div>
+          <div class="mb-3">
+            <label for="confirmPasswordP" class="form-label">Confirmar Contraseña</label>
+            <input type="password" class="form-control" name="confirmPasswordP" id="confirmPasswordP" placeholder="Confirme su contraseña" >
+          </div>
+        </div>
+        
+        <!-- Contenedor fijo para el botón de registro -->
+        <div class="button-container">
+          <button type="submit" class="btn btn-primary" style="display: none;" id="registerButton">Registrarse</button>
+          <p class="toggle-text">¿Ya tienes cuenta? <a href="#" id="showLogin">Inicia Sesión</a></p>
+        </div>
+      </form>
     
-    <script>
-  document.addEventListener('DOMContentLoaded', function() {
-    const tipoUsuarioSelect = document.getElementById('tipo_usuario');
-    const empresaContainer = document.getElementById('empresaContainer');
-    const particularContainer = document.getElementById('particularContainer');
-    const registerButton = document.querySelector('button[type="submit"]');
+      <script>
+        document.addEventListener('DOMContentLoaded', function() {
+        const tipoUsuarioSelect = document.getElementById('tipo_usuario');
+        const empresaContainer = document.getElementById('empresaContainer');
+        const particularContainer = document.getElementById('particularContainer');
+        const registerButton = document.querySelector('button[type="submit"]');
 
-    tipoUsuarioSelect.addEventListener('change', function() {
-      const selectedValue = this.value;
+        tipoUsuarioSelect.addEventListener('change', function() {
+          const selectedValue = this.value;
 
-      if (selectedValue === 'EMPRESA') {
-        empresaContainer.style.display = 'block';
-        particularContainer.style.display = 'none';
-      } else if (selectedValue === 'PARTICULAR') {
-        empresaContainer.style.display = 'none';
-        particularContainer.style.display = 'block';
-      } else {
-        empresaContainer.style.display = 'none';
-        particularContainer.style.display = 'none';
-      }
+          if (selectedValue === 'EMPRESA') {
+            empresaContainer.style.display = 'block';
+            particularContainer.style.display = 'none';
+          } else if (selectedValue === 'PARTICULAR') {
+            empresaContainer.style.display = 'none';
+            particularContainer.style.display = 'block';
+          } else {
+            empresaContainer.style.display = 'none';
+            particularContainer.style.display = 'none';
+          }
 
-      // Mostrar el botón de registro cuando se selecciona un tipo de usuario válido
-      if (selectedValue) {
-        registerButton.style.display = 'block';
-      } else {
-        registerButton.style.display = 'none';
-      }
-    });
-  });
-</script>
+          // Mostrar el botón de registro cuando se selecciona un tipo de usuario válido
+          if (selectedValue) {
+            registerButton.style.display = 'block';
+          } else {
+            registerButton.style.display = 'none';
+          }
+          });
+        });
+      </script>
 
       <script src="formateo.js"></script>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
